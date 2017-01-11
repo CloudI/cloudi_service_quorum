a337ec75dfd81ad002f62d6dfbe0cf1a608fb9e9
Michael Truog <mjtruog@gmail.com>
2017-01-10 21:01:59 -0800

    Version 1.6.0.

--- mix.exs
+++ mix.exs
@@ -3,18 +3,18 @@ defmodule CloudIServiceQuorum do
 
   def project do
     [app: :cloudi_service_quorum,
-     version: "1.5.4",
+     version: "1.6.0",
      language: :erlang,
-     description: description,
-     package: package,
-     deps: deps]
+     description: description(),
+     package: package(),
+     deps: deps()]
   end
 
   defp deps do
     [{:proper,
       [git: "https://github.com/manopapad/proper.git",
        tag: "v1.2"]},
-     {:cloudi_core, "~> 1.5.4"}]
+     {:cloudi_core, "~> 1.6.0"}]
   end
 
   defp description do
