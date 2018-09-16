7bfd8ed97f73febb5ea1d780abb780106011a4e4
Michael Truog <mjtruog@protonmail.com>
2018-09-15 21:44:10 -0700

    Version 1.7.4.

--- mix.exs
+++ mix.exs
@@ -6,8 +6,23 @@ defmodule CloudIServiceQuorum do
 
   def project do
     [app: :cloudi_service_quorum,
-     version: "1.7.3",
+     version: "1.7.4",
      language: :erlang,
+     erlc_options: [
+       :debug_info,
+       :warnings_as_errors,
+       :strict_validation,
+       :warn_bif_clash,
+       :warn_deprecated_function,
+       :warn_export_all,
+       :warn_export_vars,
+       :warn_exported_vars,
+       :warn_obsolete_guard,
+       :warn_shadow_vars,
+       :warn_unused_import,
+       :warn_unused_function,
+       :warn_unused_record,
+       :warn_unused_vars],
      description: description(),
      package: package(),
      deps: deps()]
@@ -15,7 +30,7 @@ defmodule CloudIServiceQuorum do
 
   defp deps do
     [{:proper, "~> 1.2.0"},
-     {:cloudi_core, "~> 1.7.3"}]
+     {:cloudi_core, "~> 1.7.4"}]
   end
 
   defp description do
